cc40d499708679abbb38e382aeb0ebf15729b54e
joegotflow83 <joe@absolutod.com>
2016-06-22 15:17:47 -0400

    Working on nested routes

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from dashboard.models import Student, Announcement
+from dashboard.models import Student, Announcement, Class
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    users = UserSerializer(read_only=True)
+
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = ('id', 'users', 'name_of_class')
         lookup_field = 'name_of_class'
 
 
@@ -17,7 +25,7 @@ class AnnouncementSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class UserSerializer(serializers.ModelSerializer):
+class ClassSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        model = Class
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,16 +1,18 @@
 from rest_framework import generics, viewsets
 from django.contrib.auth.models import User
 
-from dashboard.models import Student, Announcement
-from .serializers import StudentSerializer, UserSerializer, AnnouncementSerializer
+from dashboard.models import Student, Announcement, Class
+from .serializers import StudentSerializer, UserSerializer, AnnouncementSerializer, ClassSerializer
 
 
 class StudentsListCreateAPIView(generics.ListCreateAPIView):
+    """All students endpoint"""
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
 
 
 class StudentNameClassAPIView(viewsets.ModelViewSet):
+    """All students in a particular class andpoint"""
     serializer_class = StudentSerializer
     lookup_field = 'name_of_class'
 
@@ -18,18 +20,32 @@ class StudentNameClassAPIView(viewsets.ModelViewSet):
         return Student.objects.filter(name_of_class=self.kwargs['subject'])
 
 
-class UsersListAPIView(generics.ListAPIView):
+class UsersListCreateAPIView(generics.ListCreateAPIView):
+    """All users endpoint"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """Single user endpoint"""
     serializer_class = UserSerializer
 
     def get_queryset(self):
         return User.objects.filter(pk=self.kwargs['pk'])
 
 
-class AnnouncementsListCreateAPIView(generics.ListCreateAPIView):
+"""class AnnouncementsListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = AnnouncementSerializer
+    queryset = Announcement.objects.all()"""
+
+
+class AnnouncementsViewSet(viewsets.ModelViewSet):
+    """All announcements endpoint"""
     serializer_class = AnnouncementSerializer
     queryset = Announcement.objects.all()
+
+
+class ClassesViewSet(viewsets.ViewSet):
+    """All classes enpoint"""
+    serializer_class = ClassSerializer
+    queryset = Class.objects.all()
