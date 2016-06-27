7517da827f4970e4e58a4054cbf24d82025bd06d
joegotflow83 <joe@absolutod.com>
2016-06-27 10:29:24 -0400

    Working on testing now

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class AccountsTestCase(TestCase):
+    """Test that accout sign ups are working correctly"""
+    def test_student_signup(self):
+        """Test that the users signup page recieves a 200 status code"""
+        resp = self.client.get('accounts/student_signup')
+        self.assertEqual(resp.status_code, 200)
